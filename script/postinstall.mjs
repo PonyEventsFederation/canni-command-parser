95fd21168ecaa32c229ad460801f2e9b5142c6cd
Meadowsys <blazeykirin@gmail.com>
2023-10-05 22:13:05 -0700

    refinements

--- script/postinstall.mjs
+++ script/postinstall.mjs
@@ -1,8 +1,11 @@
 import cp from "child_process";
+import fs from "fs";
 
-if (process.env["npm_config_canni_command_parser_run_install"] === "true") {
+if (process.env["npm_config_canni_command_parser_skip_build"] !== "true") {
 	let extra_args = process.env["npm_config_canni_command_parser_build_args"];
 	let command = "cd packages/node-bindings && pnpm run build:release";
 	if (extra_args) command = `${command} ${extra_args}`;
+
 	cp.execSync(command);
+	fs.rmSync("target", { recursive: true });
 }
