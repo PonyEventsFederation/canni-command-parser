5dce49a0e925f6ee30cc5e069c0043c08ddf2517
Meadowsys <blazeykirin@gmail.com>
2023-10-05 20:47:26 -0700

    prefer env var over function call param

--- packages/node-bindings/native/index.mjs
+++ packages/node-bindings/native/index.mjs
@@ -1,8 +1,10 @@
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-export default (path) => require(
-	path
-		? path
-		: (process.env["CANNI_COMMAND_PARSER_NATIVE_PATH"] ?? "../build/canni-command-parser.node")
-);
+export default path => {
+	let env_var = process.env["CANNI_COMMAND_PARSER_NATIVE_PATH"];
+	let required_path = env_var
+		? env_var
+		: (path ?? "../build/canni-command-parser.node");
+	return require(required_path)
+};
