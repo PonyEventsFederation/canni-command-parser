b0ef18f0d1cf7bc939c97c5fd30f3bea6195e19f
Meadowsys <blazeykirin@gmail.com>
2023-10-05 22:05:05 -0700

    i think i got a solution?

---
+++ script/postinstall.mjs
@@ -0,0 +1,8 @@
+import cp from "child_process";
+
+if (process.env["npm_config_canni_command_parser_run_install"] === "true") {
+	let extra_args = process.env["npm_config_canni_command_parser_build_args"];
+	let command = "cd packages/node-bindings && pnpm run build:release";
+	if (extra_args) command = `${command} ${extra_args}`;
+	cp.execSync(`cd packages/node-bindings && pnpm run build:release ${extra_args}`);
+}
